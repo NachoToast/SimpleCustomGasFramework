a86020bde3bb8f5969186c20b7b66a4dbf33d401
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-07-22 18:08:23 +1200

    add `ApparelImmunityPreset` class

---
+++ Source/SCGF1.4/DefRelated/ApparelImmunityPreset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace SCGF
+{
+    public class ApparelImmunityPreset : Def
+    {
+        public List<ThingDef> apparel = new List<ThingDef>();
+
+        public ApparelImmunityPreset alsoIncludes = null;
+
+        public HashSet<ThingDef> GetAllApparelDefs()
+        {
+            HashSet<ThingDef> apparelDefs = apparel.ToHashSet();
+
+            if (alsoIncludes != null) apparelDefs.AddRange(alsoIncludes.GetAllApparelDefs());
+
+            return apparelDefs;
+        }
+    }
+}
