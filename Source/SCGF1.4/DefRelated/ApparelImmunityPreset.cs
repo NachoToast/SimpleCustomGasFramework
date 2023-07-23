0dd767e13748bb8050eebb0e8fc377a8e46ff168
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-07-23 13:34:50 +1200

    add immunity preset class

--- Source/SCGF1.4/DefRelated/ApparelImmunityPreset.cs
+++ Source/SCGF1.4/DefRelated/ApparelImmunityPreset.cs
@@ -4,12 +4,19 @@ using Verse;
 
 namespace SCGF
 {
+    /// <summary>
+    /// A predefined list of apparels that grant immunity to a gas.
+    /// </summary>
     public class ApparelImmunityPreset : Def
     {
         public List<ThingDef> apparel = new List<ThingDef>();
 
         public ApparelImmunityPreset alsoIncludes = null;
 
+        /// <summary>
+        /// Recursively creates a hashset out of all apparel defs.
+        /// </summary>
+        /// <returns></returns>
         public HashSet<ThingDef> GetAllApparelDefs()
         {
             HashSet<ThingDef> apparelDefs = apparel.ToHashSet();
