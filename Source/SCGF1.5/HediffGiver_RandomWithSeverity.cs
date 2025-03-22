584d53a200d668bc570c105069ca4ef59500780b
NachoToast <32235595+NachoToast@users.noreply.github.com>
2025-03-22 22:46:53 +1300

    add helper `HediffGiver_RandomWithSeverity`

---
+++ Source/SCGF1.5/HediffGiver_RandomWithSeverity.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using Verse;
+
+namespace SCGF
+{
+    public class HediffGiver_RandomWithSeverity : HediffGiver
+    {
+        private readonly float mtbDays;
+
+        private readonly FloatRange severityRange;
+
+        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+        {
+            float chanceFactor = ChanceFactor(pawn);
+
+            if (chanceFactor == 0f)
+            {
+                return;
+            }
+
+            List<Hediff> addedHediffs = new List<Hediff>();
+
+            if (Rand.MTBEventOccurs(mtbDays / chanceFactor, 60000f, 60f) && TryApply(pawn, addedHediffs))
+            {
+                foreach (Hediff hediff in addedHediffs)
+                {
+                    hediff.Severity = severityRange.RandomInRange;
+                }
+
+                SendLetter(pawn, cause);
+            }
+        }
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            foreach (string error in base.ConfigErrors())
+            {
+                yield return error;
+            }
+
+            if (mtbDays < 0)
+            {
+                yield return $"{nameof(mtbDays)} cannot be less than 0";
+            }
+
+            if (severityRange == null)
+            {
+                yield return $"{nameof(severityRange)} cannot be null";
+            }
+            else
+            {
+                if (severityRange.max <= 0)
+                {
+                    yield return $"{nameof(severityRange)} maximum cannot be less than or equal to 0 (got {severityRange.max})";
+                }
+                if (severityRange.min <= 0)
+                {
+                    yield return $"{nameof(severityRange)} minimum cannot be less than or equal to 0 (got {severityRange.min})";
+                }
+            }
+        }
+    }
+}
